651a696bf5dc4900df03ad41bf13a247968a0553
Blcile <jinguanbiao@outlook.com>
2017-07-17 10:55:05 +0800

    使用mysql作为是数据库

--- weibo/weibo/settings.py
+++ weibo/weibo/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'weibo.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weibo',
+        'PASSWORD': 'root',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
